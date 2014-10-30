bef1ba756952be901bb7a69ce45612da302304eb
manielse <mathiasbn@gmail.com>
2014-10-30 22:57:04 +0100

    lidt socket.io

---
+++ eventserver/src/main/java/dk/yabs/event/EventServer.java
@@ -0,0 +1,29 @@
+package dk.yabs.event;
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
+
+public class EventServer {
+    public static void main(String[] args) throws InterruptedException {
+        Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        server.addEventListener("buildevent", String.class, new DataListener<String>() {
+            @Override
+            public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
+                server.getBroadcastOperations().sendEvent("buildevent", data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+}
--- pipeline-presenter/src/main/resources/app/js/controllers.js
+++ pipeline-presenter/src/main/resources/app/js/controllers.js
@@ -1,5 +1,17 @@
-var app = angular.module('nodeApp', []);
+var app = angular.module('nodeApp', ['btford.socket-io']);
 
-app.controller("MainController", ['$scope', function ($scope) {
-    $scope.moinz = 'MOINZ FROM ANGULAR!!!'
+app.factory('mySocket', function (socketFactory) {
+//    var myIoSocket = io.connect('/',9092);
+//    mySocket = socketFactory({
+//        ioSocket: myIoSocket
+//    });
+//    return mySocket;
+});
+
+app.controller("MainController", ['$scope','mySocket', function ($scope,mySocket) {
+    $scope.moinz = 'MOINZ FROM ANGULAR!!!';
+    $scope.buildevents = [];
+    //mySocket.on('buildevent', function (ev, data) {
+    //    $scope.buildevents.push(data)
+    //})
 }]);
